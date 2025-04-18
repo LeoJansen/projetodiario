29b6eaaecd39db5fc6d53eb3cbb5fa4715138588
Léo Jansen <leo@leonardojansen.com>
2025-04-17 21:59:04 -0300

    Creates Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Form } from "@/components/Form2";
+
+import { Form } from "@/components/Form";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex">
       <main className="flex flex-col">
-        <Form/>
+    <Form />
         
       </main>
       <footer className="flex">
